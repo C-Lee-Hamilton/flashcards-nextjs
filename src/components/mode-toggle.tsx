9073f5fbb3682b2ff2474521772508e09b25ce92
Cody Lee <notlimaheelydoc@gmail.com>
2025-08-05 01:35:34 -0500

    hooked in ShadCN, got a working and persistent dark mode toggle rigged in

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useTheme } from "next-themes"
+
+import { Button } from "./ui/button";
+import { Moon, Sun } from "lucide-react"
+
+export default function ModeBtn(){
+
+
+  const { setTheme, resolvedTheme } = useTheme();
+
+
+ 
+    const toggle = () => {
+    setTheme(resolvedTheme === "light" ? "dark" : "light");
+  };
+  
+
+    return (
+        <div className="" >
+              <Button onClick = {toggle} variant="outline" className="h-[30px] w-[30px]">
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          
+        </Button>
+        
+        </div>
+    )
+}
